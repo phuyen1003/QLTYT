bf28d46e7ae689d56ae6c6d0637dc7dd43c1ca9e
phuyen1003 <110922221+phuyen1003@users.noreply.github.com>
2023-02-24 16:41:28 +0700

    meo

--- QLTYT/Controllers/HomeController.cs
+++ QLTYT/Controllers/HomeController.cs
@@ -15,14 +15,13 @@ namespace QLTYT.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
